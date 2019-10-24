f8a0b88fa8998d8404d279d95ad00cfa86cf9a85
Sihan Liu <sihanl5@student.unimelb.edu.au>
2019-10-25 06:54:53 +1100

    before add clock

---
+++ Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/MyClockPublisher.cs
@@ -0,0 +1,39 @@
+//http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
+
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class MyClockPublisher : UnityPublisher<MessageTypes.Std.Time>{
+        private MessageTypes.Std.Time stamp;
+
+        // private MessageTypes.Rosgraph.Clock clock;
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateMessage();
+        }
+        private void InitializeMessage()
+        {
+            // clock = new MessageTypes.Rosgraph.Clock();
+
+            stamp = new MessageTypes.Std.Time();
+            // Debug.Log(stamp);
+        }
+        private void UpdateMessage()
+        {
+            float time = Time.realtimeSinceStartup;
+            stamp.secs = (uint)time;
+            stamp.nsecs = (uint)(1e9 * (time - stamp.secs));
+            
+            Publish(stamp.ToString());
+            
+        }
+    }
+
+}
